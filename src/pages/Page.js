49915db9f90ba555108aeb24a9e96d9f54bd4b32
André Ribas <zitoloco@gmail.com>
2020-05-02 19:20:33 -0300

    aplica fonte manuscrita e icones

--- src/pages/Page.js
+++ src/pages/Page.js
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
 import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMale, faFemale } from '@fortawesome/free-solid-svg-icons'
 import pickOne from '../helpers/pickOne'
 import families from '../config/families'
 import maleNames from '../config/maleNames'
@@ -16,24 +18,25 @@ function Page ({ match }) {
     const allMalePrefix = [...maleNames.table_a, ...maleNames.table_b]
     const malePrefix = pickOne(allMalePrefix)
     const maleSuffix = pickOne(maleNames.suffix)
-    maleName = `masculino: ${malePrefix}${maleSuffix}`
+    maleName = malePrefix + maleSuffix
 
     const allFemalePrefix = [...femaleNames.table_a, ...femaleNames.table_b]
     const femalePrefix = pickOne(allFemalePrefix)
     const femaleSuffix = pickOne(femaleNames.suffix)
-    femaleName = `feminino: ${femalePrefix}${femaleSuffix}`
+    femaleName = femalePrefix + femaleSuffix
 
     familyName = match.params.family
-    family = `família: ${pickOne(families[familyName])}`
+    family = `${pickOne(families[familyName])}`
   }
 
   const Name = styled.div`
     background: var(--global-primary-color-${familyName});
     color: var(--global-secundary-color-${familyName});
-    font-family: "Helvetica Neue","Helvetica","Arial",sans-serif;
+    font-family: 'Shadows Into Light', cursive;
     font-size: 2rem;
     font-weight: 600;
     text-align: center;
+    letter-spacing: 3px;
     line-height: 1.125;
     padding: 16px 0;
     margin: 10px 0;
@@ -43,9 +46,8 @@ function Page ({ match }) {
   return (
     <section>
       <Nav activeFamily={match.params.family} />
-      <Name>{maleName}</Name>
-      <Name>{femaleName}</Name>
-      <Name>{family}</Name>
+      <Name><FontAwesomeIcon icon={faMale} /> {maleName} {family}</Name>
+      <Name><FontAwesomeIcon icon={faFemale} /> {femaleName} {family}</Name>
     </section>
   )
 }
