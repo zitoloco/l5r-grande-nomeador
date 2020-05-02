95b6464ebf4210baa2b7e7316b41094a885b3c35
André Ribas <zitoloco@gmail.com>
2020-05-02 14:56:49 -0300

    remove bulma framework

--- src/pages/Page.js
+++ src/pages/Page.js
@@ -1,34 +1,38 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
+import styled from 'styled-components'
 import pickOne from '../helpers/pickOne'
-
 import maleNames from '../config/maleNames'
-
 import Nav from '../components/Nav'
 
 function Page ({ match }) {
-  const allPrefix = [...maleNames.table_a, ...maleNames.table_b]
-  const prefix = pickOne(allPrefix)
-  const suffix = pickOne(maleNames.suffix)
-  const familyName = match.params.family
-  const family = pickOne(maleNames[familyName])
+  let name = 'Selecione uma familia acima'
+  let familyName
+  if (match.params.family) {
+    const allPrefix = [...maleNames.table_a, ...maleNames.table_b]
+    const prefix = pickOne(allPrefix)
+    const suffix = pickOne(maleNames.suffix)
+    familyName = match.params.family
+    const family = pickOne(maleNames[familyName])
+    name = `${prefix}${suffix} ${family}`
+  }
 
-  const name = `${prefix}${suffix} ${family}`
-
-  const familyClass = `hero ${familyName}`
+  const Name = styled.div`
+    background: var(--global-primary-color-${familyName});
+    color: var(--global-secundary-color-${familyName});
+    font-family: "Helvetica Neue","Helvetica","Arial",sans-serif;
+    font-size: 2rem;
+    font-weight: 600;
+    text-align: center;
+    line-height: 1.125;
+    padding: 3rem 1.5rem;
+    text-transform: capitalize;
+  `
 
   return (
     <section>
       <Nav activeFamily={match.params.family} />
-      <section className={familyClass}>
-        <div className="container">
-          <div className="hero-body">
-            <div className="container">
-              <h1 className="title is-capitalized">{name}</h1>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Name>{name}</Name>
     </section>
   )
 }
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-import EmptyPage from './pages/EmptyPage'
 import Page from './pages/Page'
 
 const Routes = () => (
   <Router>
     <Switch>
-      <Route exact path="/" component={EmptyPage} />
+      <Route exact path="/" component={Page} />
       <Route exact path="/:family" component={Page} />
     </Switch>
   </Router>
